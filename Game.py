dd4b8a218855358a50f46d7e169e20b2320c29cf
keone3 <78936603+keone3@users.noreply.github.com>
2021-02-24 00:12:16 -0800

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,14 +1,19 @@
 class Game():
 
-    def __init__(self, gameID, colour, openingGeneral, openingSpecific, variant, timeControl, winner):
+    def __init__(self, gameID, colour, openingGeneral, openingSpecific, variant, timeControl, incriment, winner):
         
-        self.gameID = gameID
-        self.colour = colour
-        self.openingGeneral = openingGeneral
-        self.openingSpecific = openingSpecific
-        self.variant = variant
-        self.timeControl = timeControl
-        self.winner = winner
+        self.gameID = gameID #Stores the game ID
+        self.colour = colour #Stores the colour the user played as
+        
+        #Stores the two portions of the opening name. ex) if the opening is 'Queens Gambit Accepted: Two Knights Variation' then v
+        self.openingGeneral = openingGeneral #stores 'Queens Gambit Accepted'
+        self.openingSpecific = openingSpecific #stores 'Two Knights Variation'
+        #Note: if there is no second part of the opening then openingSpecific is initialized to -1
+        
+        self.variant = variant #stores the variant ex) 'standard' 'bughouse' etc.
+        self.timeControl = timeControl #stores the time control ex) 'bullet' 
+        self.incriment = incriment #stores the time incriment
+        self.winner = winner #stores the winner. True if user won, false otherwise
     
     def getGameID(self):
         return self.gameID
@@ -27,6 +32,9 @@ class Game():
         
     def getTimeControl(self):
         return self.timeControl
+    
+    def getIncriment(self):
+        return self.incriment
         
     def getWinner(self):
         return self.winner
