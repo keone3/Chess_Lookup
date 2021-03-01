2eadcd3918d373dea8becf79b558f4bcdd715c90
Brian Currie <brian.currie@hotmail.com>
2021-02-28 21:46:57 -0800

    Fixed edge cases with user input for username and numGames, Styled game_stats.html table

--- FlaskTest.py
+++ FlaskTest.py
@@ -14,22 +14,42 @@ def home():
 # Might want to move the valid username check into a different file for more functionality in the future?
 @app.route('/', methods=['POST'])
 def home_post():
-    username = str(request.form['username'])
-    numGames = int(request.form['numGames'])
+    username = request.form['username']
+    numGames = request.form['numGames']
     usernameError = ''
-        
+    numGamesError = ''
+    badInfo = False
+
+    if(username == ''):
+        usernameError = "Username Empty"
+        badInfo = True
+    else:
+        username = str(request.form['username'])
+
+    if(numGames == ''):
+        numGamesError = "Number of Games Empty"
+        badInfo = True
+    elif(int(request.form['numGames']) < 0):
+        numGamesError = "Number of Games Must Be Positive"
+        badInfo = True
+    else:
+        numGames = int(request.form['numGames'])
+
     # Sends a get request for user data based on username
     r = requests.get('https://lichess.org/api/user/' + username)
+    
     # r.text is empty when the username is Invalid 
     if(r.text == ''):
         usernameError = "Username Invalid"
-        # Refreshes main page with error message
-        return render_template('index.html', usernameError = usernameError)
+        badInfo = True
+
     # "closed":true is in r.text if the username is closed. Example of closed username: "123123"
     if('closed' in r.text):
         usernameError = "Usernamed Closed"
-        # Refreshes main page with error message
-        return render_template('index.html', usernameError = usernameError)
+        badInfo = True
+
+    if(badInfo):
+        return render_template('index.html', numGamesError = numGamesError, usernameError = usernameError)
 
     test = LichessProject(username, numGames)
     mostCommonOpening = AnalysisAlgorithms.findMostPlayedOpening(test.gameDatabase)
