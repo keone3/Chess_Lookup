3ded4bd3156cc36ab0bc097692c08dda86f50673
keone3 <78936603+keone3@users.noreply.github.com>
2021-03-22 22:15:49 -0700

    Just updating formatting changes on my end after installing prettier

--- src/GameDatabase.js
+++ src/GameDatabase.js
@@ -1,47 +1,38 @@
-
-
-class GameDatabase{
-
-    constructor(){
-
-        this.fullGameList = new LinkedList()
-
+class GameDatabase {
+    constructor() {
+        this.fullGameList = new LinkedList();
     }
 
-    addGame(game){// game is of type Game
-        
-        let gameNode = new ListNode(game, null, null)//creates node for the game
-        this.fullGameList.addBack(gameNode)//adds game to various lists depending on the information about the game
-        
+    addGame(game) {
+        // game is of type Game
+
+        let gameNode = new ListNode(game, null, null); //creates node for the game
+        this.fullGameList.addBack(gameNode); //adds game the master list
     }
 
-    generateGameList(speed, colour){// generates a list of game of the specified game speed and colour
+    generateGameList(speed, colour) {
+        // generates and returns a list of game of the specified game speed and colour
 
-        let partitionedList = new LinkedList()
-        let curr = this.fullGameList.getHead()
-        while (curr != null){
-            if(colour == 'all'){
-                if(curr.getGame().getSpeed() == speed){
-                    let newNode = new ListNode(curr.getGame(), null, null)
-                    partitionedList.addBack(newNode)
+        let partitionedList = new LinkedList();
+        let curr = this.fullGameList.getHead();
+        while (curr != null) {
+            if (colour == "all") {
+                if (curr.getGame().getSpeed() == speed) {
+                    let newNode = new ListNode(curr.getGame(), null, null);
+                    partitionedList.addBack(newNode);
                 }
-            }else{
-                if(curr.getGame().getColour() == colour && curr.getGame().getSpeed() == speed){
-                    let newNode = new ListNode(curr.getGame(), null, null)
-                    partitionedList.addBack(newNode)
+            } else {
+                if (
+                    curr.getGame().getColour() == colour &&
+                    curr.getGame().getSpeed() == speed
+                ) {
+                    let newNode = new ListNode(curr.getGame(), null, null);
+                    partitionedList.addBack(newNode);
                 }
             }
-            curr = curr.getNext()
+            curr = curr.getNext();
         }
 
-        return partitionedList
-
+        return partitionedList;
     }
-
-    
-    
-
-
-
-
 }
--- src/LinkedList.js
+++ src/LinkedList.js
@@ -1,80 +1,80 @@
 //standard linked list implementation using ListNode
 
 class LinkedList {
-  constructor() {
-    //list does not have doubly linked nodes. Nodes have the field if it needs to be implemented
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-  }
-
-  addBack(game) {
-    //parameter is of type ListNode. Adds to the back of the list
-
-    if (this.size == 0) {
-      this.head = game;
-      this.tail = game;
-    } else {
-      this.tail.setNext(game);
-      this.tail = game;
+    constructor() {
+        //list does not have doubly linked nodes. Nodes have the 'prev' field if it needs to be implemented
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
     }
-    this.size++;
-  }
 
-  addFront(game) {
-    //parameter is of type Game. Adds to the front of the list
-    if (this.size == 0) {
-      this.head = game;
-      this.tail = game;
-    } else {
-      game.setNext(head);
-      this.head = game;
+    addBack(game) {
+        //parameter is of type ListNode. Adds to the back of the list
+
+        if (this.size == 0) {
+            this.head = game;
+            this.tail = game;
+        } else {
+            this.tail.setNext(game);
+            this.tail = game;
+        }
+        this.size++;
     }
-    this.size++;
-  }
 
-  removeBack() {
-    //removes the last element in the list
-    if (this.size < 2) {
-      this.head = null;
-      this.tail = null;
-      this.size = 0;
-    } else {
-      this.tail = tail.getPrev();
-      this.tail.setNext(null);
-      this.size--;
+    addFront(game) {
+        //parameter is of type Game. Adds to the front of the list
+        if (this.size == 0) {
+            this.head = game;
+            this.tail = game;
+        } else {
+            game.setNext(head);
+            this.head = game;
+        }
+        this.size++;
     }
-  }
 
-  removeFront() {
-    //removes the first element in the list
-    if (this.size < 2) {
-      this.size = 0;
-      this.head = null;
-      this.tail = null;
-    } else {
-      this.head = this.head.getNext();
-      this.size--;
+    removeBack() {
+        //removes the last element in the list
+        if (this.size < 2) {
+            this.head = null;
+            this.tail = null;
+            this.size = 0;
+        } else {
+            this.tail = tail.getPrev();
+            this.tail.setNext(null);
+            this.size--;
+        }
     }
-  }
 
-  clear() {
-    //empties the list
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-  }
+    removeFront() {
+        //removes the first element in the list
+        if (this.size < 2) {
+            this.size = 0;
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = this.head.getNext();
+            this.size--;
+        }
+    }
 
-  //getters
-  getHead() {
-    return this.head;
-  }
+    clear() {
+        //empties the list
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
 
-  getTail() {
-    return this.tail;
-  }
+    //getters
+    getHead() {
+        return this.head;
+    }
 
-  getSize() {
-    return this.size;
-  }
+    getTail() {
+        return this.tail;
+    }
+
+    getSize() {
+        return this.size;
+    }
 }
